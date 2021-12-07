b125f96db87c320044b1ba3ca8f4ae4ea6c4189b
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2021-12-07 18:16:14 +0000

    change logo

--- src/components/features/Logo/index.js
+++ src/components/features/Logo/index.js
@@ -1,13 +1,16 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import { HOME } from "../../../constants/routes"
+import React from "react";
+import { Link } from "react-router-dom";
+import { HOME } from "../../../constants/routes";
 
 const Logo = () => (
-    <Link to={HOME.link} className="font-bold text-white text-2xl ">
-        <span className="flex flex-col bg-brand w-10 h-10 rounded-full justify-center items-center text-center cursor-pointer
-        ">
-            t
-        </span>
-    </Link>)
+  <Link to={HOME.link} className="font-bold text-white text-2xl ">
+    <span
+      className="flex flex-col bg-brand w-10 h-10 rounded-full justify-center items-center text-center cursor-pointer
+        "
+    >
+      AI
+    </span>
+  </Link>
+);
 
-export default Logo
+export default Logo;
